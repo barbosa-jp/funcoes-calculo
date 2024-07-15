55b4bb07f7e8bfc55ee432d476ada02ee1cdcaa8
João Pedro <126364368+barbosa-jp@users.noreply.github.com>
2024-07-15 20:16:18 -0300

    Add files via upload

---
+++ funcao.js
@@ -0,0 +1,35 @@
+// Função Soma
+function soma(a, b){
+    return a + b;
+}
+console.log("Soma =",soma(4,5));
+console.log("Soma =",soma(2,3));
+
+// Função Subtração
+function subtracao(f, h){
+    return f - h;
+}
+console.log("Subtração =",subtracao(7,5));
+console.log("Subtração =",subtracao(10,5));
+
+// Função Multiplicação
+function mult(x, y = 2){
+    return x * y;
+}
+console.log("Multiplicação =",mult(5));
+console.log("Multiplicação =",mult(5,3));
+
+// Função Divisão
+function divisao(d,c){
+    return d / c;
+}
+console.log("Divisão =",divisao(10,5));
+console.log("Divisão =",divisao(3,3));
+
+// Função Potenciação
+function potencia(b,e){
+    return b ** e;
+}
+console.log("Potenciação =",potencia(2,2));
+console.log("Potenciação =",potencia(4,2));
+
